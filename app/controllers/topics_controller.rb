d80f97cd6068f7b9e5aebd402e019166751195cf
suwonkun <zhiyinzhongze@gmail.com>
2023-02-14 18:05:58 +0900

    カリキュラム用に編集

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,24 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+    @newTopic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(params[:topic].permit(:title))
+    @topic.save
+    redirect_to root_path
+  end
+
+  def delete
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    redirect_to root_path
+  end
+
+  def show
+    @topic = Topic.find(params[:format])
+    # @newpost = Post.new(:topic_id => params[:format])
+    # @posts = Post.where(topic_id: params[:format])
+  end
+end
